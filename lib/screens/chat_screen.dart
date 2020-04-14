72c5b6c8be43896bd683a17ab4b66d5e8d1f7f50
yongdo <nbmg1128@gmail.com>
2020-04-15 00:17:08 +0900

    chat image change

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,25 +1,79 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+final _fireStore = Firestore.instance;
+FirebaseUser loggedInUser; // email과 passwrod가 들어감.
 
 class ChatScreen extends StatefulWidget {
+  static String id = 'chat_screen';
+
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+
+  final messageTextController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+  String messageText;
+
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async{
+    try{
+      //현재 유저정보 가져오기
+      final user = await _auth.currentUser();
+      if(user!=null){
+        loggedInUser=user;
+      }
+    }catch(e){
+      print(e);
+    }
+  }
+
+  //getDocuments로 받으면 실시간 적용이 안된다.
+//  void getMessages() async{
+//    final messages =  await _fireStore.collection('messages').getDocuments();
+//    //messages.documents은 listview
+//    for(var meesage in messages.documents){
+//      print(meesage.data);//message.data= >  {sender: giyo1128@email.com, text: hello }
+//    }
+//  }
+
+  //snapshots은 stream임. stream으로 받으면 기다리지 않고 변화가 생기면 바로바로 변경함.
+  //_fireStore.collection('messages').snapshots()은 list임
+//  void messageStream() async {
+//    await for(var snaphost in _fireStore.collection('messages').snapshots()){//collection
+//      for(var message in snaphost.documents){//documents
+//        //print(message.data);//내용
+//      }
+//    }
+//  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: null,
-        actions: <Widget>[
+          actions: <Widget>[
           IconButton(
-              icon: Icon(Icons.close),
+            //로그아웃버튼
+            icon: FaIcon(FontAwesomeIcons.doorOpen),
               onPressed: () {
-                //Implement logout functionality
+//                messageStream();
+                _auth.signOut();
+               Navigator.pop(context);
               }),
         ],
-        title: Text('⚡️Chat'),
+        title: Text('채팅내용'),
         backgroundColor: Colors.lightBlueAccent,
       ),
       body: SafeArea(
@@ -27,6 +81,9 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            //채팅 내용
+            MessageStream(),
+            //인풋버튼
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -34,26 +91,134 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText=value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
-                  FlatButton(
+                  IconButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      //컨트롤러를 관리하에 두고 버튼을 누르면 지워지도록 설정
+                      messageTextController.clear();
+                      _fireStore.collection('messages').add({
+                        'text':messageText,
+                        'sender':loggedInUser.email,
+                        'timestamp' : DateTime.now().millisecondsSinceEpoch,
+                      });
                     },
-                    child: Text(
-                      'Send',
-                      style: kSendButtonTextStyle,
-                    ),
+                    icon: FaIcon(FontAwesomeIcons.paperPlane, color: Colors.lightBlue,),
                   ),
                 ],
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+
+class MessageStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return    StreamBuilder<QuerySnapshot>(
+      stream: _fireStore.collection('messages').snapshots(),//snapshots가 stream임
+      builder:  (context, snapshot){
+        if(!snapshot.hasData){
+          return Container(
+            width: 40,
+            height: 40,
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }//hasData는 bool값, true or false
+
+        //reversed는 마지막 부분으로 최신화면을 보여주기 위해 사용함.
+        final messages = snapshot.data.documents; //document총괄 (파베 가운데)
+
+        List<MessageBubble> messageBubbles =[];
+        for (var message in messages){
+          final messageText = message.data['text'];
+          final messageSender = message.data['sender'];
+          final messageTimeStamp = message.data['timestamp'].toString();
+
+          final currentUser = loggedInUser.email;
+
+          final messageBubble = MessageBubble(
+            text: messageText,
+            sender: messageSender,
+            isMe: currentUser==messageSender,
+            timeStamp: messageTimeStamp,
+          );
+             messageBubbles.add(messageBubble);
+             messageBubbles.sort((a, b) => b.timeStamp.compareTo(a.timeStamp));
+
+        }
+        return Expanded(
+          child: ListView(
+            //텍스트 리스트뷰를 만들고 텍스트 위젯을 넣어서 컬럼에다가 넣는다.
+            //column도 []시작하니 listview인것
+            reverse: true,//맨 아래부터 글이 나오도록
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            children:messageBubbles,
+          ),
+        );
+      },
+    );
+  }
+}
+
+
+class MessageBubble extends StatelessWidget {
+
+  final String sender;
+  final String text;
+  final String timeStamp;
+  final bool isMe;
+
+  const MessageBubble({Key key, this.sender, this.text, this.timeStamp, this.isMe}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(sender, style: TextStyle(fontSize: 12.0, color: Colors.black45),),
+          Material(
+            borderRadius:
+            isMe ?
+            BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                bottomLeft: Radius.circular(30.0),
+                bottomRight: Radius.circular(30.0)
+            )
+            :
+            BorderRadius.only(
+                topRight: Radius.circular(30.0),
+                bottomLeft: Radius.circular(30.0),
+                bottomRight: Radius.circular(30.0)
+            ),
+            elevation: 5.0,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Text(
+                  '$text',
+              style: TextStyle(
+                  fontSize: 15.0,
+                color: isMe ? Colors.white : Colors.black,
+              ),),
+            ),
+
+          ),
+        ],
       ),
     );
   }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,14 +1,70 @@
+import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'login_screen.dart';
+import 'registration_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
+  static String id = 'welcome_screen';
+
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+//하나의 애니메이션만 사용할 경우 Single를 사용함.
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin{
+
+  AnimationController animationController;
+  Animation animation;
+
+  //처음 빌드하기전에 컨트롤러를 저장하기.
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      duration: Duration(seconds: 2),//1초동안
+      vsync:this, //this는 WelcomeScreen을 의미함.
+//      upperBound: 100.0,//100까지 설정하도록
+    );
+
+    //커브애니메이션 만들기 위해서 따로 추가
+    //애니메이션컨트롤러 연결해서 현재포지션을 저장하고, curve효과를 넣는다.
+    //curve애니메이션에서는 AnimationController의 upperBound가 0~1이여야한다.
+    //대신 크기는 animation.value로 정한 이름에서 *100을 곱한다.
+   // animation = CurvedAnimation(parent: animationController, curve: Curves.decelerate);
+
+//    animationController.reverse(from: 1.0);
+    animationController.forward();//0~1사이
+
+    //forward()라면 complete을
+    //revser()라면 dismissed
+//    animation.addStatusListener((status) {
+//      if(status == AnimationStatus.completed){
+//        animationController.reverse(from: 1.0);
+//      }else if(status==AnimationStatus.dismissed){
+//        animationController.forward();
+//      }
+//    });
+
+    //트윈애니메이션
+//    animation = ColorTween(begin: Colors.white, end: Colors.lightBlueAccent).animate(animationController);
+    animationController.addListener(() {
+      setState(() {});
+      //print(animation.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+//      backgroundColor: Colors.red.withOpacity(animationController.value), 흰색->빨간색
+//      backgroundColor: animation.value,
       backgroundColor: Colors.white,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
@@ -16,59 +72,30 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Row(
-              children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
-                ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
+            Hero(
+              tag: 'logo',
+              child: Container(
+                child: Image.asset('images/waterdrop.png'),
+//                    height: animation.value*100,
+                height: 60.0,
               ),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
+            TextLiquidFill(
+              text: 'WATERDROP',
+              boxBackgroundColor: Colors.white,
+              waveColor: Colors.blueAccent,
+              textStyle: TextStyle(
+                color: Colors.white,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
               ),
+            ),
+
+            RoundedButton(title: '로그인', colour: Colors.lightBlueAccent,
+            onPressed: (){Navigator.pushNamed(context, LoginScreen.id);},
+            ),
+            RoundedButton(title: '계정 등록', colour: Colors.blueAccent,
+              onPressed: (){Navigator.pushNamed(context, RegistrationScreen.id);},
             ),
           ],
         ),
